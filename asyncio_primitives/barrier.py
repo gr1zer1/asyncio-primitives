c8d84916c5ac1305dae9c94f12413d863b5fbca2
gr1zer1 <timuravramenko99@gmail.com>
2026-06-01 13:49:45 +0200

    fix: barrier dead lock by adding generation counter

--- asyncio_primitives/barrier.py
+++ asyncio_primitives/barrier.py
@@ -1,30 +1,48 @@
+import asyncio
 from asyncio.locks import Condition
 
-
-
 class Barrier:
-
-    def __init__(self,n: int):
-        self._coroutines: int = 0
+    def __init__(self, n: int):
+        if n <= 0: 
+            raise ValueError
         self.capacity: int = n
+        
+        self._coroutines: int = 0
         self._cond: Condition = Condition()
+        
+        self._generation: int = 0  
 
-    
-async def _wait(self):
-    async with self._cond:
-        self._coroutines += 1
-        if self._coroutines == self.capacity:
+    async def wait(self):
+        async with self._cond:
 
-            self._coroutines = 0  
-            self._cond.notify_all()
-        else:
+            my_generation = self._generation
+            
+            self._coroutines += 1
+            
+            if self._coroutines == self.capacity:
 
-            while self._coroutines < self.capacity and self._coroutines != 0:
-                await self._cond.wait()
-    
+                self._coroutines = 0
+                self._generation += 1  
+
+                self._cond.notify_all()
+            else:
+                try:
+                    while my_generation == self._generation:
+
+                        await self._cond.wait()
+
+                except asyncio.CancelledError:
+                    if my_generation == self._generation:
+                        self._coroutines -= 1
+
+                        if self._coroutines == 0:
+                            self._cond.notify_all()
+
+                    raise
 
     async def __aenter__(self):
-        await self._wait()
+        await self.wait()
+        return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        ...
+        pass
