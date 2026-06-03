2ab3809526f0fa5963d0bae9d9a7799e1908b4f7
gr1zer1 <timuravramenko99@gmail.com>
2026-06-03 23:23:19 +0200

    add: async features priority queue

--- asyncio_primitives/priority_queue.py
+++ asyncio_primitives/priority_queue.py
@@ -1,6 +1,9 @@
 import heapq
 from dataclasses import dataclass, field
 from typing import Any
+from asyncio.locks import Condition
+
+
 
 
 @dataclass(order=True)    
@@ -12,19 +15,31 @@ class Instance:
 class PriorityQueue:
     def __init__(self):
         self._queue: list[Instance] = []
+        self._cond: Condition = Condition()
 
 
-    def push(self, item: Any, priority: int):
-        inst = self._create_instance(item, priority)
+    async def push(self, item: Any, priority: int):
+        async with self._cond: 
+            inst = self._create_instance(item, priority)
 
-        heapq.heappush(self._queue, inst)
+            heapq.heappush(self._queue, inst)
+
+            self._cond.notify_all()
     
 
-    def pop(self) -> Any:
-        if self.is_empty():
-            raise IndexError
-        return heapq.heappop(self._queue)
+    async def pop(self) -> Any:
+        async with self._cond:
+            while self.is_empty():
+                await self._cond.wait()
+            inst = heapq.heappop(self._queue)
+
+            self._cond.notify_all()
+
+            return inst.item
     
+
+
+
     def is_empty(self) -> bool:
         return len(self._queue) == 0
         
